a130e3d4850bd269030292b861f6e7542fc761e2
Evgenii Ivanov <ievgesha@mail.ru>
2026-01-21 15:48:43 +0700

    Fix Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,8 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function launchWelcomeForm() {
+function launchWelcomeForm(): void
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-};
+}
